ca68c019ff96abd438f916f5ae61187041acef61
MSI PC <devdodo225@gmail.com>
2024-03-21 15:37:12 +0000

    teste keky ngues

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,38 @@
 <?php
 
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Keky\Product\Http\Controllers\CollectionsController;
-use Keky\Product\Http\Controllers\ProductsController;
 
-Route::post('/products', [ProductsController::class, 'store']);
-Route::put('/products/{id}', [ProductsController::class, 'update']);
-Route::delete('/products/{id}', [ProductsController::class, 'destroy']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-//Attach a product to collections
-Route::post(
-    '/products/{id}/collections/',
-    [ProductsController::class, 'attachCollections']
-);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-// Create new collection
-Route::post('/collections', [CollectionsController::class, 'store']);
 
-//Attach a collection to products
-Route::post(
-    '/collections/{id}/products',
-    [CollectionsController::class, 'attachProducts']
-);
+    /**
+     * -------------------------------
+     *  GESTION CRUD STORAGE SYSTEM
+     * -------------------------------
+     */
+       //API endpoint to create product
+       Route::match(['GET','POST'],'creatProduct',[ProductController::class, 'creatProduct']);
+       //API endpoint to create option
+       Route::match(['GET','POST'],'creatOption',[ProductController::class, 'creatOption']);
+       //API endpoint to create option value
+       Route::match(['GET','POST'],'creatOption_value',[ProductController::class, 'creatOption_value']);
+       // API endpoint to attach one product to many options
+       Route::match(['GET','POST'],'oneproduct_to_options',[ProductController::class, 'oneproduct_to_options']);
+       //API endpoint to attach one option to many products
+       Route::match(['GET','POST'],'oneOption_to_products',[ProductController::class, 'oneOption_to_products']);
